d29b3b034e70878e8f17288f5c2c6ca2674f006d
root <root@tm-v-prov01.(none)>
2013-06-25 02:49:07 +0000

    Add adjusted resolv_conf module from QTS Puppet

---
+++ modules/resolv_conf/manifests/init.pp
@@ -0,0 +1,24 @@
+# Class: resolv_conf
+# This module manages resolv.conf file in Messagin Gateway solution
+#
+# Parameters:
+#
+# $dns_servers: DNS server that will populate /etc/resolv.conf
+# $dns_servers = [ '192.168.130.101', '192.168.130.102' ]
+#
+# Sample usage:
+#
+# class { 'resolv_conf':
+#       dns_servers => [ '192.168.130.101', '192.168.130.102' ],
+# }
+
+class resolv_conf ( $dns_servers  = [ '10.223.204.70', '10.223.204.71' ] ) {
+
+	file { '/etc/resolv.conf':
+		owner		=> root,
+		group		=> root,
+		mode		=> 0644,
+		content		=> template('resolv_conf/resolv_conf.erb'),
+	}
+
+} # End of class resolv_conf
