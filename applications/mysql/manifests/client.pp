4874268d60d388f0023d26b455a95469d8196d8a
douglas.meldrum <douglas.meldrum@tm-v-prov01.synchronica.com>
2013-11-11 19:50:26 +0000

    mysqld

---
+++ applications/mysql/manifests/client.pp
@@ -0,0 +1,10 @@
+# client.pp
+
+class mysql::client {
+
+	package { "MySQL-client":
+		ensure		=> "${version}",
+		provider	=> yum,
+	}	
+
+} # End of class mysql::client:w
---
+++ applications/mysql/manifests/server.pp
@@ -0,0 +1,41 @@
+# Class: mysql::server
+# This module install the Mysql Client and Server.
+#
+# Parameters:
+#
+# $version: MySql version.
+# $version         = '5.5.28-1.rhel5'
+#
+# Sample usage:
+#
+# class { 'mysql::server':
+#       version                 => '5.5.28-1.rhel5',
+#       stage                   => post,
+# }
+
+class mysql::server (   $version                = '5.5.28-1.rhel5' ) {
+
+	# We include the client rpm to the MySQL server too.
+	include mysql::client, mysql::config, mysql::service
+
+	package { "MySQL-devel":
+		ensure		=> "${version}",
+		provider	=> yum,
+	}	
+
+	package { "MySQL-server":
+		ensure		=> "${version}",
+		provider	=> yum,
+	}	
+
+	package { "MySQL-shared":
+		ensure		=> "${version}",
+		provider	=> yum,
+	}	
+
+	package { "MySQL-shared-compat":
+		ensure		=> "${version}",
+		provider	=> yum,
+	}	
+
+} # End of class mysql::server
