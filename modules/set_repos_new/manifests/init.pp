b2f09371bd760af0a5d17bcb7ffe59dfc9fd3200
douglas.meldrum <douglas.meldrum@tm-v-prov01.synchronica.com>
2013-11-11 18:23:25 +0000

    Repo configuration

--- modules/set_repos_new/manifests/init.pp
+++ modules/set_repos_new/manifests/init.pp
@@ -20,53 +20,32 @@
 #       my_role => 'tbd',
 # }
 
-class set_repos_new ( 	$yum_server1  = '172.16.36.101',
-			$unity_branch = '1.0',
-			$my_role = 'tbd' ) {
+class set_repos_new ( 	$yum_server1  = 'tm-v-repos.synchronica.com' ) {
 
 	file {  ['/etc/yum.repos.d/CentOS-Base.repo', '/etc/yum.repos.d/CentOS-Debuginfo.repo', '/etc/yum.repos.d/kickstart.repo',
 		'/etc/yum.repos.d/rhel4.8.repo', '/etc/yum.repos.d/CentOS-Media.repo', '/etc/yum.repos.d/CentOS-Vault.repo']:
 		ensure		=> absent,
 	}
 
+	yumrepo { 'Base':
+		baseurl		=> "http://${yum_server1}/${operatingsystem}/${lsbdistrelease}/os/${architecture}",
+		descr		=> 'Base Packages',
+		enabled		=> 1,
+		gpgcheck	=> 0,
+	}
+
 	yumrepo { 'Custom':
-		baseurl		=> "http://${yum_server1}/yum/${operatingsystem}/${lsbdistrelease}/custom/${architecture}",
+		baseurl		=> "http://${yum_server1}/${operatingsystem}/${lsbdistrelease}/custom/${architecture}",
 		descr		=> 'Custom Packages',
 		enabled		=> 1,
 		gpgcheck	=> 0,
 	}
 
 	yumrepo { 'Errata':
-		baseurl		=> "http://${yum_server1}/yum/${operatingsystem}/${lsbdistrelease}/errata/${architecture}",
+		baseurl		=> "http://${yum_server1}/${operatingsystem}/${lsbdistrelease}/errata/${architecture}",
 		descr		=> 'Errata Packages',
 		enabled		=> 1,
 		gpgcheck	=> 0,
-	}
-
-	yumrepo { 'Base':
-		baseurl         => $lsbdistrelease ? {
-					/^5/ => "http://${yum_server1}/yum/${operatingsystem}/${lsbdistrelease}/base/${architecture}/${operatingsystem}",
-					/^4/ => "http://${yum_server1}/yum/${operatingsystem}/${lsbdistrelease}/base/${architecture}/${operatingsystem}/RPMS",
-		},
-		descr		=> 'Base Packages',
-		enabled		=> 1,
-		gpgcheck	=> 0,
-	}
-
-	yumrepo { 'Kickstart':
-		baseurl		=> "http://${yum_server1}/yum/${operatingsystem}/${lsbdistrelease}/kickstart/${architecture}",
-		descr		=> 'Kickstart Packages',
-		enabled		=> 1,
-		gpgcheck	=> 0,
-	}
-
-	if $unity_branch != '1.0' {
-		yumrepo { 'Application':
-			baseurl		=> "http://${yum_server1}/yum/Application/${unity_branch}/${my_role}",
-			descr		=> 'Application Packages',
-			enabled		=> 1,
-			gpgcheck	=> 0,
-		}
 	}
 
 } # End of class set_repos_new
