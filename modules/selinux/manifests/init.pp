98a978ee3f8b327ac3fc52c1317c973d363fd288
douglas.meldrum <douglas.meldrum@tm-v-prov01.synchronica.com>
2013-12-02 15:13:27 +0000

    Enforcing permissive mode in selinux

--- modules/selinux/manifests/init.pp
+++ modules/selinux/manifests/init.pp
@@ -1,4 +1,9 @@
 # init.pp
+#
+# NOTE: This manifest will always for selinux into permissive mode
+#	regardless of its configuration. You will need to change
+#	the manifest's exec to setenforce 1 to change this.
+#	- Douglas 12.02.13
 
 class selinux {
 
@@ -7,6 +12,10 @@ class selinux {
                 group		=> root,
                 mode		=> 0644,
                 source		=> 'puppet:///modules/selinux/config',
+        }
+        exec { '/usr/sbin/setenforce 0':
+                refreshonly     => true,
+                subscribe       => File['/etc/selinux/config'],
         }
 
 }
