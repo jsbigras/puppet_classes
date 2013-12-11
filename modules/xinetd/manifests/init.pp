20fc931854634b225105fb4d9edc9ab291b040f7
douglas.meldrum <douglas.meldrum@tm-v-prov01.synchronica.com>
2013-12-11 19:08:45 +0000

    Ensure that xinetd is running

--- modules/xinetd/manifests/init.pp
+++ modules/xinetd/manifests/init.pp
@@ -6,4 +6,15 @@ class xinetd {
                 ensure          => present,
                 provider        => yum,
         }
+        service { 'xinetd':
+                ensure          => running,
+                enable          => true,
+                require         => Exec['xinetd-reload'],
+        }
+
+        exec { 'xinetd-reload':
+                command         => '/etc/init.d/xinetd reload',
+                refreshonly     => true,
+                require         => Package['xinetd'],
+        }
 }
