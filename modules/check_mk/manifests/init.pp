e2a412d68a45f5383d5806e742a972b9b7e55926
douglas.meldrum <douglas.meldrum@tm-v-prov01.synchronica.com>
2013-12-11 19:17:43 +0000

    Adding check_mk files and xinetd config to the fray

---
+++ modules/check_mk/manifests/init.pp
@@ -0,0 +1,18 @@
+# init.pp
+
+class check_mk {
+
+        file { '/etc/xinetd.d/check_mk':
+                owner		=> root,
+                group		=> root,
+                mode		=> 0644,
+                source		=> 'puppet:///modules/check_mk/check_mk',
+        }
+        file { ' /usr/bin/check_mk_agent':
+                owner		=> root,
+                group		=> root,
+                mode		=> 0755,
+                source		=> 'puppet:///modules/check_mk/check_mk_agent',
+        }
+
+}
